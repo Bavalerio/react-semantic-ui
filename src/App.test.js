4dbc2d8de4196093da48830e31d3b8c753bd79b1
Bryan Valerio <bryanvale91@gmail.com>
2020-05-05 18:30:58 -0600

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('should renders form', () => {
+  const wrapper = (<App />);
+  expect(wrapper.type('Button'))
 });
