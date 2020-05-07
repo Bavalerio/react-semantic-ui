ebd7211e384fdc9f8b92b22fc6d8b59197192eaf
Bryan Valerio <bryanvale91@gmail.com>
2020-05-07 14:26:30 -0600

    add post

--- semantic/src/App.js
+++ semantic/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './navigation';
+import Posts from './posts'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Posts />
     </div>
   );
 }
--- semantic/src/navigation.js
+++ semantic/src/navigation.js
@@ -4,9 +4,9 @@ import { Menu } from 'semantic-ui-react';
 function NavigationBar() {
   return (
     <Menu pointing secondary>
-      <Menu.Item name="home" href="/somelink" />
-      <Menu.Item name="post" href="/somelink" />
-      <Menu.Item name="contact" href="somelink" />
+      <Menu.Item icon="home" name="home" href="/somelink" />
+      <Menu.Item icon="box" name="post" href="/somelink" />
+      <Menu.Item icon="address book" name="contact" href="somelink" />
     </Menu>
   );
 }
